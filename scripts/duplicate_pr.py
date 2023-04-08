3e612276f1256e5c05e95e3957efb478a9338be9
Jonathan Tang <Jonahtang738@gmail.com>
2023-04-08 01:03:08 -0700

    test

--- scripts/duplicate_pr.py
+++ scripts/duplicate_pr.py
@@ -56,7 +56,7 @@ def get_new_commits(base_branch: str, curr_branch:str, base_commit:str = None):
         git_fetch_branch(base_branch)
     print("base commit is", base_commit)
     if base_commit:
-        base_commit = git.show("{}^1".format(base_commit))
+        base_commit = git.show("{}^1".format(base_commit)).split()[1]
         print("in if, new base commit is", base_commit)
         print("in if, new base commit split is", base_commit.split())
     else:
